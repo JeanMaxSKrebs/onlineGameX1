ad35cbac267009ef7981e5e78682e48ad79dbc07
JeanMaxSKrebs <JeanMaxSKrebs>
2020-02-21 11:12:09 -0300

    2 players controlers funcionando

--- main.js
+++ main.js
@@ -1,35 +1,152 @@
+class controle {
+
+    constructor(personagem, UP, DOWN, LEFT, RIGHT, TIRO, estaEsquerda) {
+        this.personagem = personagem
+        this.UP = UP
+        this.DOWN = DOWN
+        this.LEFT = LEFT
+        this.RIGHT = RIGHT
+        this.TIRO = TIRO
+        this.moveEsquerda = false;
+        this.moveDireita = false;
+        this.moveCima = false;
+        this.moveBaixo = false;
+        this.atirar = false;
+        this.estaEsquerda = estaEsquerda
+    }
+    // comecaAtirar(e) {
+    //     var key = e.keycode;
+    //     console.log("o")
+
+    //     if (key === this.TIRO) {
+    //         this.atirar = true
+    //     }
+    // }
+    // paraAtirar(e) {
+    //     var key = e.keycode;
+    //     console.log("i")
+
+    //     if (key === this.TIRO) {
+    //         this.atirar = false
+    //     }
+    // }
+
+    comecaMovimento(e) {
+        var key = e.keyCode;
+        if (key === this.LEFT && key !== this.RIGHT) {
+            this.moveEsquerda = true
+        }
+        if (key === this.RIGHT && key !== this.LEFT) {
+            this.moveDireita = true
+        }
+        if (key === this.UP && key !== this.DOWN) {
+            this.moveCima = true
+        }
+        if (key === this.DOWN && key !== this.UP) {
+            this.moveBaixo = true
+        }
+        /* tiro */
+        if (key === this.TIRO) {
+            this.atirar = true
+        }
+    }
+    paraMovimento(e) {
+        var key = e.keyCode;
+        if (key === this.LEFT && key !== this.RIGHT) {
+            this.moveEsquerda = false;
+        }
+        if (key === this.RIGHT && key !== this.LEFT) {
+            this.moveDireita = false;
+        }
+        if (key === this.UP && key !== this.DOWN) {
+            this.moveCima = false;
+        }
+        if (key === this.DOWN && key !== this.UP) {
+            this.moveBaixo = false;
+        }
+        /* tiro */
+        if (key === this.TIRO) {
+            this.atirar = false
+        }
+    }
+    move() {
+        let newX = this.personagem.x;
+        let newY = this.personagem.y;
+        let speed = this.personagem.speed;
+        let lowSpeed = speed / this.personagem.reducaoVelocidade;
+
+        if (this.moveEsquerda && !this.atirar) {
+            newX -= speed;
+        }
+        if (this.moveDireita && !this.atirar) {
+            newX += speed;
+        }
+        if (this.moveCima && !this.atirar) {
+            newY -= speed;
+        }
+        if (this.moveBaixo && !this.atirar) {
+            newY += speed;
+        }
+        if (this.atirar) {
+            if (this.personagem.projetil.length <= 20) {
+                this.personagem.meioX = this.x + this.personagem.larguraPersonagem / 2
+                this.personagem.meioY = this.y + this.personagem.alturaPersonagem / 2
+
+                this.personagem.projetil.push({
+                    x: this.personagem.x + (this.personagem.larguraPersonagem / 2) - (this.personagem.larguraProjetil / 2),
+                    y: this.personagem.y + (this.personagem.alturaPersonagem / 2) - (this.personagem.alturaProjetil / 2)
+                })
+                
+            }
+        }
+        if (this.moveEsquerda && this.atirar) {
+            newX -= lowSpeed;
+        }
+        if (this.moveDireita && this.atirar) {
+            newX += lowSpeed;
+        }
+        if (this.moveCima && this.atirar) {
+            newY -= lowSpeed;
+        }
+
+        if (this.moveBaixo && this.atirar) {
+            newY += lowSpeed;
+        }
+        this.personagem.atualizarPosicao(newX, newY)
+        this.personagem.moveProjetil(this.estaEsquerda)
+    }
+}
 
 const Player1 = new personagem(100, (canvas.height - pixel * 2) / 2, true)
 
 const Player2 = new personagem(canvas.width - 100, (canvas.height - pixel * 2) / 2, false)
 
-const ControlerPlayer1 = new controle(Player1, 38, 40, 37, 39, 32)
+const ControlerPlayer1 = new controle(Player1, 87, 83, 65, 68, 32, Player1.estaEsquerda)
+console.log(Player1.estaEsquerda)
 
-const ControlerPlayer2 = new controle(Player2, 87, 83, 65, 68, 13)
+const ControlerPlayer2 = new controle(Player2, 38, 40, 37, 39, 13, Player2.estaEsquerda)
+console.log(Player2.estaEsquerda)
+
+function comecarMovimento(e) {
+    ControlerPlayer1.comecaMovimento(e)
+    ControlerPlayer2.comecaMovimento(e)
+}
+function pararMovimento(e) {
+    ControlerPlayer1.paraMovimento(e)
+    ControlerPlayer2.paraMovimento(e)
+}
+
+window.addEventListener("keydown", comecarMovimento);
+window.addEventListener("keyup", pararMovimento);
+
 
 async function loop() {
     limpaImprimeVerifica()
 
     ControlerPlayer1.move()
-
     ControlerPlayer2.move()
-
-    Player1.moveProjetil()
-
-    Player2.moveProjetil()
 
     requestAnimationFrame(loop)
 }
 
 requestAnimationFrame(loop)
-
-
-// let definicoes = {
-//     alturaPersonagem: pixel * 2,
-//     larguraPersonagem: pixel,
-//     hp: 100,
-//     dano: 20,
-//     speed: 20,
-//     x: 100,
-//     y: (canvas.height - pixel * 2) / 2
-// }
--- personagem.js
+++ personagem.js
@@ -2,87 +2,106 @@
 // console.log(inimigo(tipo))
 
 function personagem(x, y, estaEsquerda) {
+
     console.log("personagem esta a esquerda?", estaEsquerda)
 
-
+    this.estaEsquerda = estaEsquerda
     /* fica no servidor */
-        /* posição */
-        this.x = x
-        this.y = y
-        /* aspectos fisicos */
-        this.width = 32 * 2
-        this.height = 32 * 2
-        this.hp = 100
-        this.hpMaximo = 100
-        this.lifeSteal = 10
-        this.lifeSteal = 0
-        this.speed = 10
-        this.dano = 10
-        this.larguraPersonagem = pixel
-        this.alturaPersonagem = pixel * 2
-        /* projetil */
-        this.projetil = []
-        this.larguraProjetil = pixel/3
-        this.alturaProjetil = pixel/3
-        this.reducaoVelocidade = 1.3
+    /* posição */
+    this.x = x
+    this.y = y
+    /* aspectos fisicos */
+    this.width = 32 * 2
+    this.height = 32 * 2
+    this.hp = 100
+    this.hpMaximo = 100
+    this.lifeSteal = 10
+    this.lifeSteal = 0
+    this.speed = 10
+    this.dano = 10
+    this.larguraPersonagem = pixel
+    this.alturaPersonagem = pixel * 2
+    /* projetil */
+    this.projetil = []
+    this.larguraProjetil = pixel / 3
+    this.alturaProjetil = pixel / 3
+    this.reducaoVelocidade = 1.3
 
     // console.log(this.width)
 
 
 
-    this.atualizarPosicao = function(x,y){
+    this.atualizarPosicao = function (x, y) {
         this.x = x
         this.y = y
     }
-    
-    this.desenha = function() {
+
+    this.desenhaProjetil = function () {
+        for (var i = 0; i < this.projetil.length; i++) {
             contexto.restore()
             contexto.fillStyle = "white"
-            contexto.fillRect(this.x, this.y, this.larguraPersonagem, this.alturaPersonagem)
-            
-            contexto.restore()
-            contexto.font = "20px Arial"
-            contexto.fillStyle = "white"
-            contexto.fillText("HP: " + this.hp, 70, 20)
-            contexto.fillText("Dano: " + this.dano, 70, 40)
-    }
-    this.verificaColisaoBorda = function() {
-        if(this.x<=0)
-        {
-            this.x = this.x+this.speed
-        }
-        if(this.x>=canvas.width-this.larguraPersonagem)
-        {
-            this.x = this.x-this.speed
-        }
-        if(this.y<=0)
-        {
-            this.y = this.y+this.speed
-        }
-        if(this.y>=canvas.height-this.alturaPersonagem)
-        {
-            this.y = this.y-this.speed
+            contexto.fillRect(this.projetil[i].x, this.projetil[i].y, this.larguraProjetil, this.alturaProjetil)
         }
     }
-    this.verificaColisaoInimigo = function() {
+
+    this.moveProjetil = function (estaEsquerda) {
+        console.log(estaEsquerda)
+        if(estaEsquerda){
+            for (var i = 0; i < this.projetil.length; i++) {
+                this.projetil[i].x = this.projetil[i].x + this.speed
+            }
+        }else
+        {
+            for (var i = 0; i < this.projetil.length; i++) {
+                this.projetil[i].x = this.projetil[i].x - this.speed
+            }
+        }
+    }
+
+    this.desenha = function () {
+        contexto.restore()
+        contexto.fillStyle = "white"
+        contexto.fillRect(this.x, this.y, this.larguraPersonagem, this.alturaPersonagem)
+
+        contexto.restore()
+        contexto.font = "20px Arial"
+        contexto.fillStyle = "white"
+        contexto.fillText("HP: " + this.hp, 70, 20)
+        contexto.fillText("Dano: " + this.dano, 70, 40)
+    }
+    this.verificaColisaoBorda = function () {
+        if (this.x <= 0) {
+            this.x = this.x + this.speed
+        }
+        if (this.x >= canvas.width - this.larguraPersonagem) {
+            this.x = this.x - this.speed
+        }
+        if (this.y <= 0) {
+            this.y = this.y + this.speed
+        }
+        if (this.y >= canvas.height - this.alturaPersonagem) {
+            this.y = this.y - this.speed
+        }
+    }
+    this.verificaColisaoInimigo = function () {
         if (this.x < Inimigo.x + Inimigo.larguraInimigo &&
             this.x + this.larguraPersonagem > Inimigo.x &&
             this.y < Inimigo.y + Inimigo.alturaInimigo &&
             this.y + this.alturaPersonagem > Inimigo.y) {
 
-                this.hp = this.hp - Inimigo.dano
-                Inimigo.hp = Inimigo.hp - Player1.dano                
-                return true
-            }
-            else{
-                return false
-            }
+            this.hp = this.hp - Inimigo.dano
+            Inimigo.hp = Inimigo.hp - Player1.dano
+            return true
+        }
+        else {
+            return false
+        }
     }
-    this.verificaMorte = function() {
-        if(this.hp>0){
+    this.verificaMorte = function () {
+        if (this.hp > 0) {
             this.status = "vivo"
             return false
-        }else{
+        } else {
             this.status = "morto"
             this.x = 0 - this.alturaPersonagem
             this.y = 0 - this.larguraPersonagem
@@ -90,56 +109,36 @@ function personagem(x, y, estaEsquerda) {
             contexto.font = "48px Arial"
             canvas.style.backgroundColor = "black"
             contexto.fillStyle = "red"
-            contexto.fillText("GAME OVER", canvas.width/3, canvas.height/2)
+            contexto.fillText("GAME OVER", canvas.width / 3, canvas.height / 2)
             return true
         }
     }
-    this.rouboHP = function() {
+    this.rouboHP = function () {
 
-        if(this.hp<this.hpMaximo)
-        {
-            this.hp = this.hp + (this.dano*this.lifeSteal/100)
+        if (this.hp < this.hpMaximo) {
+            this.hp = this.hp + (this.dano * this.lifeSteal / 100)
         }
     }
 
-    this.desenhaProjetil = function() {
-        for(var i = 0 ; i < this.projetil.length ; i++ ) {
-            contexto.restore()
-            contexto.fillStyle = "white"
-            contexto.fillRect(this.projetil[i].x, this.projetil[i].y, this.larguraProjetil, this.alturaProjetil)
-        }
-    }
+    this.colisaoProjetilBorda = function () {
 
-    this.moveProjetil = function() {
-        
-        for(var i = 0 ; i < this.projetil.length ; i++ ) {
-            this.projetil[i].x = this.projetil[i].x + this.speed
-        }
-    }
-
-    this.colisaoProjetilBorda = function() {
-        
-        for(var i = 0 ; i < this.projetil.length ; i++ )
-        {
-            if(this.projetil[i].x > canvas.width - this.larguraProjetil)
-            {
+        for (var i = 0; i < this.projetil.length; i++) {
+            if (this.projetil[i].x > canvas.width - this.larguraProjetil || this.projetil[i].x <= 0) {
                 this.projetil.shift()
             }
         }
     }
-    this.colisaoProjetilInimigo = function() {
-        for(var i = 0 ; i < this.projetil.length ; i++ )
-        {
-            if(this.projetil[i].x < Inimigo.x + Inimigo.larguraInimigo &&
-               this.projetil[i].x + this.larguraProjetil > Inimigo.x &&
-               this.projetil[i].y < Inimigo.y + Inimigo.alturaInimigo &&
-               this.projetil[i].y + this. alturaProjetil > Inimigo.y)
-            {
+    this.colisaoProjetilInimigo = function () {
+        for (var i = 0; i < this.projetil.length; i++) {
+            if (this.projetil[i].x < Inimigo.x + Inimigo.larguraInimigo &&
+                this.projetil[i].x + this.larguraProjetil > Inimigo.x &&
+                this.projetil[i].y < Inimigo.y + Inimigo.alturaInimigo &&
+                this.projetil[i].y + this.alturaProjetil > Inimigo.y) {
                 this.projetil.shift()
-                Inimigo.hp = Inimigo.hp - Player1.dano                
+                Inimigo.hp = Inimigo.hp - Player1.dano
                 return true
             }
-            else{
+            else {
                 return false
             }
         }
