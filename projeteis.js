6a98a93f603372b68806928a245f21e7346cae25
Jean Max S Krebs <jeanmaxskrebs@gmail.com>
2020-04-11 17:18:47 -0300

    mudança da classe projeteis para o arquivo projetil.js

--- projeteis.js
+++
@@ -1,7 +0,0 @@
-/* definições dos projeteis */
-const arrProjeteis = []
-const larguraProjetil = pixel / 3
-const alturaProjetil = pixel / 3
-const reducaoVelocidade = 1.5
-const speedProjetil = 15
-const qtdMaximaProjeteis = 20
